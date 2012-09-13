31f041cbddbd8b1f45ab376cd95bcd57c2f56794
Aaron Smith <aaron@aaron-lee.me>
2012-09-12 17:18:26 -0700

    Added a possible menu bar. People will have to decide if they like it.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Health Track</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
@@ -20,12 +20,42 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
-
+        <div class="container">
+            <div class="navbar navbar-fixed-top">
+              <div class="navbar-inner">
+                <a class="brand" href="#">&nbsp;Health Track</a>
+                <ul class="nav">
+                    <li class="active"><a href="#">Dashboard</a></li>
+                    <li><a href="#">Patient Records</a></li>
+                    <li><a href="#">Physician Scheduler</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Order Tracking <b class="caret"></b></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Pharmacy</a></li>
+                            <li><a href="#">Lab</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Insurance Billing</a></li>
+                    <li><a href="#">Equipment and Inventory</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Administration <b class="caret"></b></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">My Account</a></li>
+                            <li><a href="#">Manage Users</a></li>
+                            <li><a href="#">Manage Permissions</a></li>
+                        </ul>
+                    </li>
+                </ul>
+              </div>
+            </div>
+        </div>
         
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
         <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/vendor/knockout-1.8.1.min.js"></script>
+
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
